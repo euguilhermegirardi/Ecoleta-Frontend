e9a7c215eec4502fe5234fa2edc76136b73f7785
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-27 17:04:43 +0100

    git add .

---
+++ src/pages/CreatePoint/index.tsx
@@ -0,0 +1,293 @@
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import axios from "axios";
+import { LeafletMouseEvent } from "leaflet";
+
+import "./styles.css";
+import logo from "../../assets/logo.svg";
+import api from "../../services/api";
+
+// array or object = manually inform the variable type
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
+const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState("0");
+
+  const [cities, setCities] = useState<string[]>([]);
+  const [selectedCity, setSelectedCity] = useState("0");
+
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    whatsapp: "",
+  });
+
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    api.get("items").then((res) => {
+      // console.log(response);
+      setItems(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((res) => {
+        const ufInitials = res.data.map((uf) => uf.sigla);
+        // console.log(ufInitials);
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === "0") {
+      return;
+    }
+
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((res) => {
+        const cityNames = res.data.map((city) => city.nome);
+        // console.log(ufInitials);
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(position);
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
+
+  // ChangeEvent<HTMLSelectElement> to select the 'targe.value'
+  function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+
+    setSelectedUf(uf);
+  }
+
+  function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+
+    setSelectedCity(city);
+  }
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    console.log(event.latlng);
+    setSelectedPosition([event.latlng.lat, event.latlng.lng]);
+  }
+
+  function inputChange(event: ChangeEvent<HTMLInputElement>) {
+    // console.log(event.target.name, event.target.value);
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSelectItem(id: number) {
+    // console.log(id);
+    const alreadySelected = selectedItems.findIndex((item) => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    // console.log("testing submit...");
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items,
+    };
+
+    // console.log(data);
+    await api.post("points", data);
+
+    alert("Collect point registered!");
+    history.push("/");
+  }
+
+  return (
+    <>
+      <div id="page-create-point">
+        <header>
+          <img src={logo} alt="Ecolete-logo" />
+
+          <Link to="/">
+            <FiArrowLeft />
+            Go back to Home
+          </Link>
+        </header>
+
+        <form onSubmit={handleSubmit}>
+          <h1>
+            Register the <br /> collect point
+          </h1>
+
+          <fieldset>
+            <legend>
+              <h2>Info</h2>
+            </legend>
+
+            <div className="field">
+              <label htmlFor="name">Company name</label>
+              <input type="text" name="name" id="name" onChange={inputChange} />
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="name">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={inputChange}
+                />
+              </div>
+
+              <div className="field">
+                <label htmlFor="name">WhatsApp</label>
+                <input
+                  type="text"
+                  name="whatsapp"
+                  id="whatsapp"
+                  onChange={inputChange}
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Address</h2>
+              <span>Select the address in the map</span>
+            </legend>
+
+            <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={selectedPosition} />
+            </Map>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">State (UF)</label>
+                <select
+                  name="uf"
+                  id="uf"
+                  value={selectedUf}
+                  onChange={handleSelectedUf}
+                >
+                  <option value="0">Select an UF</option>
+                  {ufs.map((uf) => (
+                    <option key={uf} value={uf}>
+                      {uf}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="field">
+                <label htmlFor="city">City</label>
+                <select
+                  name="city"
+                  id="city"
+                  value={selectedCity}
+                  onChange={handleSelectedCity}
+                >
+                  <option value="0">Select a city</option>
+                  {cities.map((city) => (
+                    <option key={city} value={city}>
+                      {city}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Collect Items</h2>
+              <span>Select one or more items below</span>
+            </legend>
+            <ul className="items-grid">
+              {items.map((item) => (
+                <li
+                  key={item.id}
+                  onClick={() => handleSelectItem(item.id)}
+                  className={selectedItems.includes(item.id) ? "selected" : ""}
+                >
+                  <img src={item.image_url} alt="item.title" />
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+          </fieldset>
+
+          <button type="submit">Register a collect point</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default CreatePoint;
